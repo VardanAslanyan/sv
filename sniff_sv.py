a3a6c63aa16daa1c644a4213cfbcb204d851480e
Vardan <var.aslanyan@future.am>
2021-06-08 13:52:54 +0400

    bug fixing, while True removed

--- sniff_sv.py
+++ sniff_sv.py
@@ -7,29 +7,32 @@ def sniffer_data(host=socket.gethostbyname(socket.gethostname()), port=None, des
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as d:
             s.bind((host, port))
-            s.listen(5)
+            s.listen(10)
             conn, address = s.accept()
             d.connect((destination, destination_port))
             with conn:
-                while True:
-                    data = conn.recv(1024)
-                    data_hex = binascii.hexlify(data)
-                    if not data:
-                        break
-                    try:
-                        SV(data_hex.decode("utf-8")).__repr__()
-                    except Exception as ex:
-                        print(ex)
-                    finally:
-                        d.sendall(data)
-                        to_client = d.recv(1024)
-                        to_client_hex = binascii.hexlify(to_client)
-                    try:
-                        SV(to_client_hex.decode("utf-8")).__repr__()
-                    except Exception as ex:
-                        print(ex)
-                    finally:
-                        conn.sendall(to_client)
+                # while True:
+                data = conn.recv(1024)
+                data_hex = binascii.hexlify(data)
+                # if not data:
+                #     break
+                try:
+                    SV(data_hex.decode("utf-8")).__repr__()
+                except Exception as ex:
+                    print(ex)
+                finally:
+                    d.sendall(data)
+                    to_client = d.recv(1024)
+                    to_client_hex = binascii.hexlify(to_client)
+                try:
+                    SV(to_client_hex.decode("utf-8")).__repr__()
+                except Exception as ex:
+                    print(ex)
+                finally:
+                    conn.sendall(to_client)
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     dest_address = "91.199.226.7"
     dest_port = 10010
     while True:
-        sniffer_data(port=5050, destination=dest_address, destination_port=dest_port)
+        sniffer_data(port=10010, destination=dest_address, destination_port=dest_port, host="0.0.0.0")
