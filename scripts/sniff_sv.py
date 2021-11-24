c90f281d068d181d6e96caf320b6c6ddce1512d3
VardanAslanyan <var.aslanyan@gmail.com>
2021-11-24 14:34:29 +0400

    added some print lines

--- scripts/sniff_sv.py
+++ scripts/sniff_sv.py
@@ -17,9 +17,13 @@ def sniffer_data(host=socket.gethostbyname(socket.gethostname()), port=None,
     d = None
     with conn:
         while True:
-            data = conn.recv(1024)
-            listen_data.settimeout(30)  # TODO check work correct or not
-            print(data)
+            try:
+                data = conn.recv(1024)
+                listen_data.settimeout(30)  # TODO check work correct or not
+                print("\nRequest>>>", data)
+            except Exception as error:
+                print(error)
+                break
             if not data:
                 print("Good bye!")
                 break
@@ -29,7 +33,6 @@ def sniffer_data(host=socket.gethostbyname(socket.gethostname()), port=None,
 
                 d = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 d.connect((destination, destination_port))
-
                 # for auto-reversal not to send ArCa
                 # print(data)
                 # if b'040020' in data:
@@ -46,13 +49,10 @@ def sniffer_data(host=socket.gethostbyname(socket.gethostname()), port=None,
             except Exception as ex:
                 print(ex)
                 break
-            # finally:
-
             try:
+                print("\n\nResponse>>>", to_client)
                 to_client_hex = binascii.hexlify(to_client)
                 SV(to_client_hex.decode("utf-8")).__repr__()
-
-                print(to_client)
                 if scenario == 'nothing_pass_to_pos':
                     if b'210r' in to_client:
                         print('\n-----------------Created automatic_reversal nothing_pass_to_pos--------------\n')
@@ -70,19 +70,11 @@ def sniffer_data(host=socket.gethostbyname(socket.gethostname()), port=None,
             except Exception as ex:
                 print(ex)
                 break
-            # finally:
-
-
-                # print(to_client)
-                # if b'0210r' in to_client:
-                #     x = to_client.replace(b'0003401', b'1163401')
-                #     conn.sendall(x)
-                # else:
-                #     conn.sendall(to_client)
-
             print("The end!")
-    d.close()
-    listen_data.close()
+    if d:
+        d.close()
+    if listen_data:
+        listen_data.close()
     print('It is the end!')
 
 
